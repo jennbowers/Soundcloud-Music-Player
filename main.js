1d61bd59840ab86211309f464c874485df338ece
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-22 21:27:13 -0400

    finished Week 4 project, got audio player to work and play the correct song, the DOM to clear out when another search is performed, and search bar clears when selected

--- main.js
+++ main.js
@@ -4,92 +4,113 @@
 
 (function() {
 
-  // delcaring variables
-  var formNode = document.getElementById('search-form');
-  var searchBar = document.getElementById('search-bar');
-  var allResults = document.querySelector('.all-results');
-  var audioPlayer = document.querySelector('.music-player');
-  var userSearchInput = '';
-  var headers = {};
-  var tracksUrl = 'https://api.soundcloud.com/tracks/?client_id=' + SC_TOKEN + '&q=';
-
-  try {
-      headers['Authorization'] = 'token ' + SC_TOKEN;
-    } catch (e) {
-      //ignore error
-    }
-
-  // adding event listener to form
-  formNode.addEventListener('submit', function(event) {
-    event.preventDefault();
-    userSearchInput = searchBar.value;
-    userSearchInputString = userSearchInput.toString();
-    console.log(userSearchInputString);
-    console.log('Artist: ', userSearchInputString);
-    tracksUrl = tracksUrl + userSearchInputString;
-
-    fetch(tracksUrl, {headers: headers}).then(function(response){
-      console.log(tracksUrl);
-      response.json().then(function(data){
-        console.log(data);
-
-      // looping through all results to create tracks below
-      for(var i = 0; i < data.length; i++) {
-        var info = data[i];
-        var streamingInfo = {};
-
-        // each result div
-        var resultNode = document.createElement('a');
-        resultNode.setAttribute('class', 'result')
-        resultNode.setAttribute('href', '#');
-        allResults.appendChild(resultNode);
-        resultNode.addEventListener('click', function(event) {
-          audioPlayer.setAttribute('src', info.stream_url + '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f');
-        });
-
-        // artwork
-        var artworkNode = document.createElement('div');
-        artworkNode.setAttribute('class', 'artwork');
-        artworkNode.innerHTML = '<img src="' + info.artwork_url + '">';
-        resultNode.appendChild(artworkNode);
-
-        // song title
-        var titleNode = document.createElement('p');
-        titleNode.setAttribute('class', 'song-title');
-        titleNode.textContent = info.title;
-        resultNode.appendChild(titleNode);
+    // delcaring variables
+    var formNode = document.getElementById('search-form');
+    var searchBar = document.getElementById('search-bar');
+    var allResults = document.querySelector('.all-results');
+    var audioPlayer = document.querySelector('.music-player');
+    var userSearchInput = '';
+    searchBar.value = '';
+    var headers = {};
+    var tracksUrl = 'https://api.soundcloud.com/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=';
 
 
-      }
-
-      });
-
+    searchBar.addEventListener('click', function(e){
+      searchBar.value = '';
     });
 
-    // resetting variables
-    userSearchInput = ' ';
-    tracksUrl = 'https://api.soundcloud.com/tracks/?client_id=' + SC_TOKEN + '&q=';
+    // try {
+    //   headers['Authorization'] = 'token ' + SC_TOKEN;
+    // } catch (e) {
+    //   //ignore error
+    // }
 
-  });
+    // const SC_TOKEN = '095fe1dcd09eb3d0e1d3d89c76f5618f';
 
-
-
-
-    // SC.get('/tracks', {q: 'userSearchInput'}).then(function(tracks) {
-    //   console.log(tracks)
-    // });
-
-// what SoundCloud had in API documentation on how to search
-    // SC.get('/tracks', {
-    //   q: 'buskers', license: 'cc-by-sa'
-    // }).then(function(tracks) {
-    //   console.log(tracks);
-    // });
+    function fetchStream(url) {
+      fetch(url).then(function(result) {
+        console.log(result);
 
 
+        audioPlayer.src = '';
+        audioPlayer.src = result.url;
+        audioPlayer.play();
+      });
+    }
+
+    // adding event listener to form
+    formNode.addEventListener('submit', function(event) {
+        event.preventDefault();
+        userSearchInput = searchBar.value;
+        userSearchInputString = userSearchInput.toString();
+        var userSearchInputNoSpace = encodeURI(userSearchInputString);
+        tracksUrl = tracksUrl + userSearchInputNoSpace;
+        console.log(tracksUrl);
+
+        fetch(tracksUrl).then(function(response) {
+            console.log(tracksUrl);
+            response.json().then(function(data) {
+
+                //clear screen with each search, code found on SO https://stackoverflow.com/questions/683366/remove-all-the-children-dom-elements-in-div
+                while(allResults.firstChild) {
+                    allResults.removeChild(allResults.firstChild);
+                }
+
+
+                // looping through all results to create tracks below
+                for (var i = 0; i < data.length; i++) {
+                  var info = data[i];
+                  // var streamingInfo = {};
+
+                  // each result div
+                  var resultNode = document.createElement('a');
+                  // resultNode.setAttribute('class', 'result' + i)
+                  console.log(info.stream_url);
+                  resultNode.setAttribute('class', 'result')
+                  resultNode.setAttribute('id', info.stream_url + '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f');
+                  allResults.prepend(resultNode);
+                  // adding event listener
+                  console.log("LAST RESULT NODE ID" + resultNode.id);
+                  resultNode.addEventListener('click', function(event) {
+                    console.log(arguments);
+                    console.log(resultNode.id);
+                    fetchStream(event.target.id);
+                  });
+
+
+                  // artwork
+                  var artworkNode = document.createElement('div');
+                  artworkNode.setAttribute('class', 'artwork');
+                  if (info.artwork_url == null) {
+                    // image source: https://yt3.ggpht.com/-zK8v1xKkZtY/AAAAAAAAAAI/AAAAAAAAAAA/SmyGR2XCwXw/s900-c-k-no-mo-rj-c0xffffff/photo.jpg
+                    artworkNode.innerHTML = `<img src="images/youtube-record.jpg" id=${info.stream_url + '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f'}>`;
+                  } else {
+                    artworkNode.innerHTML = `<img src="${info.artwork_url}" id=${info.stream_url + '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f'}>`;
+                  }
+                  resultNode.appendChild(artworkNode);
+
+                  // song title
+                  var titleNode = document.createElement('p');
+                  titleNode.setAttribute('class', 'song-title');
+                  titleNode.textContent = info.title;
+                  resultNode.appendChild(titleNode);
+                }
+
+
+              });
+            });
+
+            tracksUrl = 'https://api.soundcloud.com/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=';
+
+
+          });
 
 
 }());
+
+
+
+
 // 1. First select and store the elements you'll be working with
 
 
